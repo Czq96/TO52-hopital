0d181094e61e76dfa0b7307ef08c95454cdb1f95
Czq96 <ohce69cc@gmail.com>
2019-03-11 23:50:26 +0100

    add package of json

--- WebSite1/App_Code/ExcelReaderDataTable.cs
+++ WebSite1/App_Code/ExcelReaderDataTable.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+// using 
 
 /// <summary>
 /// ExcelReaderDataTable 的摘要说明
 /// </summary>
 public class ExcelReaderDataTable
 {
-    public ExcelReaderDataTable()
-    {
-        //
-        // TODO: 在此处添加构造函数逻辑
-        //
-    }
+    //public DataTable ReadExcelToDataTable(string path)
+    //{
+    //    //
+    //    // TODO: 在此处添加构造函数逻辑
+    //    //
+    //}
 }
